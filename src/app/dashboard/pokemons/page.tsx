31be7d75ba401a61b7c706fce3a499f23aca090a
Nobbisaurio <davidxdcastro@gmail.com>
2024-09-13 00:55:50 -0500

    paginas staticas

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -6,7 +6,7 @@ import { PokemonGrids } from '../../../pokemons/components/PokemonGrids';
 
 
 
-export const getPokemons = async ( limit = 20, offset = 0 ): Promise<SimplePokemon[]> => {
+ export const getPokemons = async ( limit = 20, offset = 0 ): Promise<SimplePokemon[]> => {
   const data: PokemonsResponse = await fetch( `https://pokeapi.co/api/v2/pokemon?offset=${ offset }&limit=${ limit }` )
     .then( res => res.json() );
 
@@ -30,7 +30,7 @@ export default async function PokemonsPage() {
 
 
   return (
-    <div className='flex flex-col'>
+    <div className='p-2 flex flex-col'>
 
       <span className='text-5xl my-2'>Listado de pokemons estatico</span>
 
